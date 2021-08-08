f884fc0ee880daecfbe15e8daca8021bd3a598b9
Asric <asric@163.com>
2021-08-08 20:29:33 +0800

    add

---
+++ BrithdayRemind/BrithdayRemind/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrithdayRemind
+//
+//  Created by 王闯 on 2021/8/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
